1cffb24f2d1d2cd8d5c6aeafffff829414412b55
furqon.ragda <furqon.ragda@gmail.com>
2023-03-22 14:14:42 +0700

    init project

---
+++ HR Service/Data/ApiDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HR_Service.Data
+{
+    public class ApiDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApiDBContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("HRServiceDB"));
+        }
+
+    }
+}
